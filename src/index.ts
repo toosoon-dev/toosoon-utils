85eea42fe9df5a048a174dc5217b40fc981d8bd2
Rocca <arnaud@toosoon.paris>
2024-02-19 12:57:23 +0100

    fix: export namings

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export * from './colors';
-export * from './dom';
-export * from './files';
-export * from './functions';
-export * from './geometry';
-export * from './maths';
-export * from './now';
-export * from './random';
-export * from './strings';
+export * as colors from './colors';
+export * as dom from './dom';
+export * as files from './files';
+export * as functions from './functions';
+export * as geometry from './geometry';
+export * as maths from './maths';
+export * as now from './now';
+export * as random from './random';
+export * as strings from './strings';
 
-export * from './constants';
-export * from './types';
+export * as constants from './constants';
+export type * as types from './types';
